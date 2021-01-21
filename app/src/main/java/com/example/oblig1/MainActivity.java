b33e1b11772cbdc8ab7b8d84c8a7044fa1a610b7
h576347 <42640829+h576347@users.noreply.github.com>
2021-01-21 08:39:47 +0100

    Har lagt inn uimplementerte koder(skallet) for knappene i MainActivity

--- app/src/main/java/com/example/oblig1/MainActivity.java
+++ app/src/main/java/com/example/oblig1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.oblig1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+
+        Button btn1 = (Button)findViewById(R.id.button1);       //The first button is created´"Ta quiz"
+        btn1.setOnClickListener(new View.OnClickListener() {    //The first buttons action is created
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+
+        Button btn2 = (Button)findViewById(R.id.button2);       //The second button is created "Legg til bilde"
+        btn2.setOnClickListener(new View.OnClickListener() {    //The second button´s action is created
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+
+}}
