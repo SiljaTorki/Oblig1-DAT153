cdbacbc060725cbd885bb040e754ae185bb63255
h576347 <42640829+h576347@users.noreply.github.com>
2021-01-21 08:42:53 +0100

    Har lagt inn uimplementerte koder(skallet) for knappene i quiz

--- app/src/main/java/com/example/oblig1/Quiz.java
+++ app/src/main/java/com/example/oblig1/Quiz.java
@@ -3,6 +3,8 @@ package com.example.oblig1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Quiz extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class Quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+
+        Button btn3 = (Button)findViewById(R.id.button3);       //The third button is created´"Sjekk svar"
+        btn3.setOnClickListener(new View.OnClickListener() {    //The third buttons action is created
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+
+        Button btn4 = (Button)findViewById(R.id.button4);       //The fourth button is created "Neste"
+        btn4.setOnClickListener(new View.OnClickListener() {    //The fourth button´s action is created
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
     }
 }
