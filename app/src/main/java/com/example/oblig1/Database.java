323a96be0ada5097159ec7292d462102ecfbd403
h576347 <42640829+h576347@users.noreply.github.com>
2021-01-21 09:08:59 +0100

    Opprettet en HashMap i database, men den er tom

--- app/src/main/java/com/example/oblig1/Database.java
+++ app/src/main/java/com/example/oblig1/Database.java
@@ -2,7 +2,10 @@ package com.example.oblig1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+
+import java.util.HashMap;
 
 public class Database extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class Database extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+
+        HashMap<Image,String> hm = new HashMap<Image,String>(); //Hashmap to find correct name for each image
+
     }
 }
