226866d7d7dce74d00677e28f6dea6c7128a3512
h576347 <42640829+h576347@users.noreply.github.com>
2021-01-21 12:55:06 +0100

    Har lagt inn endel kommentarer, samt navigering fra main-> quiz. Har også prøvd å legge inn enn "correct" - toast i Quiz-klassen, men fikk ikke appen til å kjøre, så har kommentert det ut.

--- app/src/main/java/com/example/oblig1/MainActivity.java
+++ app/src/main/java/com/example/oblig1/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.oblig1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +16,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         Button btn1 = (Button)findViewById(R.id.button1);       //The first button is created´"Ta quiz"
         btn1.setOnClickListener(new View.OnClickListener() {    //The first buttons action is created
             public void onClick(View v) {
-                // Perform action on click
+                //this should send the user to the quiz-activity
+                Intent intent = new Intent(MainActivity.this, Quiz.class);
+                startActivity(intent);
             }
         });
 
@@ -26,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         Button btn2 = (Button)findViewById(R.id.button2);       //The second button is created "Legg til bilde"
         btn2.setOnClickListener(new View.OnClickListener() {    //The second button´s action is created
             public void onClick(View v) {
-                // Perform action on click
+                // this should make it possible for user to add images from the phone´s library
             }
         });
 
--- app/src/main/java/com/example/oblig1/Quiz.java
+++ app/src/main/java/com/example/oblig1/Quiz.java
@@ -2,9 +2,13 @@ package com.example.oblig1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Quiz extends AppCompatActivity {
 
@@ -13,11 +17,27 @@ public class Quiz extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        EditText editText = (EditText) findViewById(R.id.editText1);
+        int correct = 0;
+
 
         Button btn3 = (Button)findViewById(R.id.button3);       //The third button is created´"Sjekk svar"
         btn3.setOnClickListener(new View.OnClickListener() {    //The third buttons action is created
             public void onClick(View v) {
-                // Perform action on click
+                if(editText != null){
+                    /*
+                    CharSequence text = "Correct Answer!";
+                    Context context = getApplicationContext();
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    */
+
+                    //check if editText equals the text connected with the photo
+                    //if it is correct, then a toast saying its correct, is going to appear
+                    //if incorrect, the toast should say it is wrong
+                    //correct++;
+                }
             }
         });
 
@@ -25,7 +45,8 @@ public class Quiz extends AppCompatActivity {
         Button btn4 = (Button)findViewById(R.id.button4);       //The fourth button is created "Neste"
         btn4.setOnClickListener(new View.OnClickListener() {    //The fourth button´s action is created
             public void onClick(View v) {
-                // Perform action on click
+                //Jumps to the next image
+                // on the last image, it should provide a score
             }
         });
     }
