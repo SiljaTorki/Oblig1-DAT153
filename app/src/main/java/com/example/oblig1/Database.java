22ff28c1cf3dc3393ee08f7334cfb43b4fdaf502
h576347 <42640829+h576347@users.noreply.github.com>
2021-01-28 11:26:23 +0100

    Fikset database-greiene igjen hehe

--- app/src/main/java/com/example/oblig1/Database.java
+++ app/src/main/java/com/example/oblig1/Database.java
@@ -13,25 +13,27 @@ import java.util.HashMap;
 
 public class Database extends AppCompatActivity {
 
-    private ArrayList<Cat> catList = new ArrayList<>();
+
+    private ArrayList<Cat> catList = new ArrayList<>();  // A list to store the cats photos in
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
 
-        HashMap<Image,String> hm = new HashMap<Image,String>(); //Hashmap to find correct name for each image
+       // HashMap<Image,String> hm = new HashMap<Image,String>(); //Hashmap to find correct name for each image is not in use
 
-        Button btn1 = (Button)findViewById(R.id.buttonToAdd);       //The first button is created´"Ta quiz"
+        Button btn1 = (Button)findViewById(R.id.buttonToAdd);       //The first button is created´"Legg til"
         btn1.setOnClickListener(new View.OnClickListener() {    //The first buttons action is created
             public void onClick(View v) {
-                //this should send the user to the quiz-activity
+                //this should send the user to the add-activity
                 Intent intent = new Intent(Database.this, Add.class);
                 startActivity(intent);
             }
         });
     }
 
+    //Method to add new cat-photos and names
     public void addCat(Cat cat) {
         catList.add(cat);
     }
