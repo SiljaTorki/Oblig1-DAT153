295c069d8cdbbaf79e6be62d83a37625e15b7169
h576347 <42640829+h576347@users.noreply.github.com>
2021-02-01 16:37:35 +0100

    Ordnet med checkbox

--- app/src/main/java/com/example/oblig1/CustomAdapter.java
+++ app/src/main/java/com/example/oblig1/CustomAdapter.java
@@ -8,6 +8,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,10 +18,14 @@ public class CustomAdapter extends ArrayAdapter<Cat> {
     private ArrayList<Cat> cats;
     Context context;
 
+    // Boolean array for storing the state of the checkbox
+    boolean[] checkBoxState;
+
     public CustomAdapter(ArrayList<Cat> cats, Context context) {
         super(context, R.layout.list_item, cats);
         this.cats = cats;
         this.context = context;
+        checkBoxState = new boolean[cats.size()];
     }
 
     @Override
@@ -41,6 +46,7 @@ public class CustomAdapter extends ArrayAdapter<Cat> {
     private static class ViewHolder {
         TextView tv;
         ImageView img;
+        CheckBox cb;
     }
 
 
@@ -51,6 +57,7 @@ public class CustomAdapter extends ArrayAdapter<Cat> {
         // Check if an existing view is being reused, otherwise inflate the view
         ViewHolder viewHolder; // view lookup cache stored in tag
 
+
         final View result;
 
         if (convertView == null) {
@@ -58,8 +65,10 @@ public class CustomAdapter extends ArrayAdapter<Cat> {
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.list_item, parent, false);
+            //cache the views
             viewHolder.tv = (TextView) convertView.findViewById(R.id.textViewName);
             viewHolder.img = (ImageView) convertView.findViewById(R.id.imageViewCats);
+            viewHolder.cb = (CheckBox) convertView.findViewById(R.id.checkBox1);
 
             result = convertView;
 
@@ -69,9 +78,23 @@ public class CustomAdapter extends ArrayAdapter<Cat> {
             result = convertView;
         }
 
-
+        //Set the data to be displayed
         viewHolder.tv.setText(cat.getNavn());
         viewHolder.img.setImageBitmap(cat.getBilde());
+        viewHolder.cb.setChecked(checkBoxState[position]);
+
+
+        //Listens to changes
+        viewHolder.cb.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if(((CheckBox)v).isChecked())
+                    checkBoxState[position]=true;
+                else
+                    checkBoxState[position]=false;
+
+            }
+        });
 
         // Return the completed view to render on screen
         return convertView;
