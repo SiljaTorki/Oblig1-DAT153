2c605da8f0af7c6ba53ebd696407236eb12a6e53
h131177 <h131177@stud.hvl.no>
2021-02-14 23:57:37 +0100

    Lagt til dependencies

--- app/src/androidTest/java/com/example/oblig1/buttonTest.java
+++ app/src/androidTest/java/com/example/oblig1/buttonTest.java
@@ -1,10 +1,10 @@
 package com.example.oblig1;
 
-import androidx.test.espresso.core.internal.deps.guava.collect.Iterables;
+//import androidx.test.espresso.core.internal.deps.guava.collect.Iterables;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-//import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterables;
 
 import org.junit.After;
 import org.junit.Before;
@@ -14,11 +14,10 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+//import static androidx.test.ext.truth.content.IntentSubject.assertThat;
+
+
 
 @RunWith(AndroidJUnit4.class)
 public class buttonTest {
@@ -46,6 +45,6 @@ public class buttonTest {
                 .perform(click());
         // An intent is fired to launch a different Activity. Robolectric doesn't currently
         // support launching a new Activity, so use Espresso Intents to verify intent was sent
-        //assertThat(Iterables.getOnlyElement(Intents.getIntents())).hasComponentClass(Quiz.class);
+        assertThat(Iterables.getOnlyElement(Intents.getIntents())).hasComponentClass(Quiz.class);
     }
 }
