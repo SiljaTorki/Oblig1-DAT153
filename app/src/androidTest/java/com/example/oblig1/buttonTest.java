bdec5514646f278b69fbd81bd5c59e13b3455c1b
h576347 <42640829+h576347@users.noreply.github.com>
2021-02-15 13:09:37 +0100

    Ble frønt i "button"-klassen... men aner ikke om det er riktig skrevet

--- app/src/androidTest/java/com/example/oblig1/buttonTest.java
+++ app/src/androidTest/java/com/example/oblig1/buttonTest.java
@@ -14,7 +14,13 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 //import static androidx.test.ext.truth.content.IntentSubject.assertThat;
 
 
@@ -38,13 +44,27 @@ public class buttonTest {
         Intents.release();
     }
 
+
     @Test
     public void pushButton() {
         //onView(withText("Hello world!")).check(matches(isDisplayed()));
         onView(withId(R.id.buttonQuiz))
+                .check(matches((isDisplayed())));
+
+        onView(withId(R.id.buttonQuiz))
                 .perform(click());
+
+        intended(
+                hasComponent(Quiz.class.getName())
+                //assertThat(Iterables.getOnlyElement(Intents.getIntents())).hasComponentClass(Quiz.class);
+        );
+
+
+
+        //assertTrue(withId(R.id.buttonQuiz).hasComponany(Quiz.class));
+
         // An intent is fired to launch a different Activity. Robolectric doesn't currently
         // support launching a new Activity, so use Espresso Intents to verify intent was sent
-        assertThat(Iterables.getOnlyElement(Intents.getIntents())).hasComponentClass(Quiz.class);
+       // assertThat(Iterables.getOnlyElement(Intents.getIntents())).hasComponentClass(Quiz.class);
     }
 }
