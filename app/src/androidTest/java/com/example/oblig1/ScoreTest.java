e3c1da345b60f41a2d88f431faac82b6426d0ff6
h576347 <42640829+h576347@users.noreply.github.com>
2021-02-15 13:49:41 +0100

    Ett forsøk på å sjekke om viewet blir riktig oppdatert

--- app/src/androidTest/java/com/example/oblig1/ScoreTest.java
+++ app/src/androidTest/java/com/example/oblig1/ScoreTest.java
@@ -3,6 +3,7 @@ package com.example.oblig1;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,8 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class ScoreTest {
@@ -22,14 +25,26 @@ public class ScoreTest {
 
     @Test
     public void scoreIsCorrect() {
-        onView(withId(R.id.editText1))
-                .perform(typeText("Cat one"), closeSoftKeyboard());
 
-        onView(withId(R.id.buttonSvar))
-                .perform(click());
+            //Providing an answer for a random image in Quiz.class
+            onView(withId(R.id.editText1))
+                    .perform(typeText("Cat one"), closeSoftKeyboard());
 
-      //  onView(withId(R.id.quizScore))
-        //        .check();
+            //Clicks the button i order to check whether the answer is correct or not
+            onView(withId(R.id.buttonSvar))
+                    .perform(click());
+
+            /*
+            * If it is correct guessed, then the score should update to 1
+            * otherwise it should not be updated
+            * only the first image is tested
+             */
+            if(onView(withId(R.id.imageView)).equals("Cat one")){
+                assertTrue(onView(withId(R.id.quizScore)).equals("Your score: 1"));
+            }else{
+                assertFalse(onView(withId(R.id.quizScore)).equals("Your score: 1"));
+            }
+
 
 
     }
