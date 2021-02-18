d476b643b48fd37e45476b32ee353061bc40494a
h576347 <42640829+h576347@users.noreply.github.com>
2021-02-18 12:10:14 +0100

    Laget flere metoder i quiz.java. Har også sjekket at det fungerer

--- app/src/main/java/com/example/oblig1/Quiz.java
+++ app/src/main/java/com/example/oblig1/Quiz.java
@@ -19,6 +19,10 @@ import com.example.oblig1.helpers.*;
 
 import java.util.List;
 
+/**
+* The class is used to show the cat-images, and to update the score if the user guess correctly
+*
+ */
 
 public class Quiz extends AppCompatActivity {
 
@@ -26,7 +30,7 @@ public class Quiz extends AppCompatActivity {
     TextView count;
 
     private List<Cat> cats;
-    private static String catName;
+    private static String catName;  //Used for testing only
     private QuizHelp quizh = new QuizHelp(); //Calling for the quiz-helping class
 
     private int counter = 1;
@@ -37,7 +41,9 @@ public class Quiz extends AppCompatActivity {
 
     private Button btnCheckAnswer;
     private Button btnNext;
+
     private ImageView image;
+    private EditText editText;
 
     private DatabaseHelper dbHelper;
 
@@ -46,7 +52,82 @@ public class Quiz extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        //gets access to the database
         dbHelper = new DatabaseHelper(getApplicationContext());
+
+        theToolbar();
+
+        getFromDB();
+
+        scoreAndCount();
+
+        //Finding the user´s input
+        editText = (EditText) findViewById(R.id.editTextGuess);
+
+        //Getting the ImageView
+        image = (ImageView) findViewById(R.id.imageViewQuiz);
+
+        //Implementing the buttons
+        btnCheckAnswer = (Button)findViewById(R.id.buttonCheckAnswer);
+        btnNext = (Button)findViewById(R.id.buttonNext);
+
+        image.setImageURI(Uri.parse(cats.get(i).getImage()));
+
+
+        /*
+        * Checking the answer of the user´s input, by using a lambda expression
+        * using the method answerAndResponse()
+         */
+        btnCheckAnswer.setOnClickListener((View v) -> {
+            answerAndResponse();
+        });
+
+        /*
+        * Getting the next image in the list
+         * using the method updateQuiz()
+         */
+        btnNext.setOnClickListener((View v) -> {
+            updateQuiz();
+
+        });
+
+    }
+
+    /*
+    * Setting the score and count the first time
+    * Everything except max is set to zero
+     */
+    private void scoreAndCount(){
+        // Tracking question
+        count = findViewById(R.id.quizCounter);
+        String quizCount = counter + "/" + max;
+
+        //Provides a score at the top of the application
+        score = findViewById(R.id.quizScore);
+        String quizScore = "Your score: " + quizh.getCorrect();
+
+        count.setText(quizCount);
+        score.setText(quizScore);
+    }
+
+
+    /*
+     * Gets the catList from the database with help from DatabaseHelper.java
+     * Generates a random list of cats with help from QuizHelp.java
+     */
+    private void getFromDB(){
+        //Getting the list of cats from the database
+        cats = dbHelper.getAllCats();
+        cats = quizh.randomList(cats);
+        max = cats.size();
+
+        //Only used for testing
+        catName = cats.get(0).getName();
+    }
+
+
+    //Make it possible for the user to get back to the MainActivity.class
+    private void theToolbar(){
         // my_toolbar is defined in the layout file
         Toolbar myChildToolbar =
                 (Toolbar) findViewById(R.id.my_toolbar_quiz);
@@ -58,97 +139,59 @@ public class Quiz extends AppCompatActivity {
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
 
-        //Finding the user´s input
-        EditText editText = (EditText) findViewById(R.id.editTextGuess);
-
-        //Getting the image
-        image = (ImageView) findViewById(R.id.imageViewQuiz);
-
-        //Finding the "Sjekk svar" and "Neste" button i the view
-        btnCheckAnswer = (Button)findViewById(R.id.buttonCheckAnswer);       //The button´"Sjekk svar"
-        btnNext = (Button)findViewById(R.id.buttonNext);            //The button "Neste"
-
-        //Getting the list of cats from the database
-        cats = dbHelper.getAllCats();
-        cats = quizh.randomList(cats);
-        max = cats.size();
-        catName = cats.get(0).getName();
-
-        // Tracking question
-        count = findViewById(R.id.quizCounter);
-        String quizCount = counter + "/" + max;
-
-
-        //Provides a score at the top of the application
-        score = findViewById(R.id.quizScore);
-        String quizScore = "Your score: " + quizh.getCorrect();
-
-        //Used for the toasts
-        Context context = getApplicationContext();
-        int duration = Toast.LENGTH_LONG;
-        String noImage = "No quiz available!";
-
-        count.setText(quizCount);
-        score.setText(quizScore);
-
-        showOrHide();
-
-        //Checking the answer of the user´s input, by using a lambda expression
-        btnCheckAnswer.setOnClickListener((View v) -> {
-
-           //A response to the user, correct/wrong answer
-            CharSequence response = null;
-
-            /*
-            * if not empty, gets the text from editText field
-            * and checking the user´s answer
-            */
-            if(editText != null) {
-                String answer = editText.getText().toString();
-                response = quizh.checkAnswer(answer, cats.get(i).getName());
-            }
-
-                //Updating the score by calling getCorrect() from QuizHelper
-                String quizScore1 = "Your score: " + quizh.getCorrect();
-                score.setText(quizScore1);
-
-                //Showing the toast
-                Toast toast = Toast.makeText(context, response, duration);
-                toast.show();
-
-        });
-
-
-        //The click-method goes to the next image, by using lambda expression
-        btnNext.setOnClickListener((View v) -> {
-
-            //Jumps to the next image but stops at the last one
-            if (i < cats.size()-1) {
-                i++;
-                counter++;
-
-                //Updates the counter
-                String quizCount1 = counter + "/" + max;
-                count.setText(quizCount1);
-
-                //Finding the new image
-                image.setImageURI(Uri.parse(cats.get(i).getImage()));
-
-
-                //Empty the editText field
-                editText.getText().clear();
-            }
-        });
-
     }
 
-    public void showOrHide(){
-        // Gets the first image from the list
-        if(max > 0)
 
+    //Finds and show the next image in the list
+    private void updateQuiz(){
+        //Jumps to the next image but stops at the last one
+        if (i < cats.size()-1) {
+            i++;
+            counter++;
+
+            //Updates the counter
+            String quizCount1 = counter + "/" + max;
+            count.setText(quizCount1);
+
+            //Finding the new image
             image.setImageURI(Uri.parse(cats.get(i).getImage()));
 
+            //Empty the editText field
+            editText.getText().clear();
+        }
     }
+
+
+    /*
+    * Checking the users answer, with help from QuizHelp.java
+    * Updates the score if correct guess
+    * Also provides a response in form of a Toast
+    */
+    private void answerAndResponse(){
+        CharSequence response = null;
+        /*
+         * if not empty, gets the text from editText field
+         * and checking the user´s answer
+         */
+        if(editText != null) {
+            String answer = editText.getText().toString();
+            response = quizh.checkAnswer(answer, cats.get(i).getName());
+        }
+
+        //Updating the score by calling getCorrect() from QuizHelper
+        String quizScore1 = "Your score: " + quizh.getCorrect();
+        score.setText(quizScore1);
+
+        //Showing the toast
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, response, duration);
+        toast.show();
+
+    }
+
+
+    //Only used for testing
     public static String getCatName() {
         return catName;
     }
