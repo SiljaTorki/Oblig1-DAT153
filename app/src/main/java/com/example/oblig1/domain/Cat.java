7f33202ba01bf6cb4ffccda922a5b000610840b6
h576347 <42640829+h576347@users.noreply.github.com>
2021-02-18 10:29:14 +0100

    Lagt inn egen primary key som er en int for kattene. Så nå kan kattene ha samme navn uten problemer

--- app/src/main/java/com/example/oblig1/domain/Cat.java
+++ app/src/main/java/com/example/oblig1/domain/Cat.java
@@ -8,26 +8,18 @@ import androidx.room.PrimaryKey;
 @Entity(tableName = "Cat")
 public class Cat {
 
-    //@NonNull
-    //@PrimaryKey(autoGenerate = true)
-    //public int id;
     @NonNull
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name="catName")
     private String name;
 
-    //TODO: finne en måte å kunne ha bitmap her, men uten at den skal komme seg inn i databasen
-    //private Bitmap image;
     @ColumnInfo(name="image")
     private String image;
 
-    //Images are usually stored as BLOB data in the database
-//    @ColumnInfo(typeAffinity = ColumnInfo.BLOB)
- //   private byte[] byteImage;
-
-
     public Cat(String name, String image) {
         this.name = name;
-        //this.byteImage = byteImage;
         this.image = image;
     }
 
@@ -35,9 +27,6 @@ public class Cat {
         return name;
     }
 
-    /*public byte[] getByteImage() {
-        return byteImage;
-    }*/
 
     public String getImage() {
         return image;
