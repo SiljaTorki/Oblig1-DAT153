df225cab52c5287bc5382b3d82c3acbeaaf05419
h576347 <42640829+h576347@users.noreply.github.com>
2021-02-18 10:41:24 +0100

    Fjernet utkommenteringer

--- app/src/main/java/com/example/oblig1/MainActivity.java
+++ app/src/main/java/com/example/oblig1/MainActivity.java
@@ -1,26 +1,18 @@
 package com.example.oblig1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.oblig1.domain.Cat;
 import com.example.oblig1.helpers.DatabaseHelper;
-import com.example.oblig1.sqlLite.AppDatabase;
-
-import java.util.List;
 
 //import com.example.oblig1.domain.CatList;
 
 public class MainActivity extends AppCompatActivity {
 
-   // final String DRAWABLE = "android.resource://com.example.oblig1/drawable/";
-
-   // public final static String DATABASE = "catDatabase";
     private DatabaseHelper dbHelper;
 
     @Override
@@ -28,40 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //If catList is empty, the three original cat-images should be added to the catList
 
-       /* AppDatabase catDatabase = Room.databaseBuilder(
-                getApplicationContext(),
-                AppDatabase.class,
-                DATABASE).build();
-*//*
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-
-                    try {
-                        List<Cat> cats = catDatabase.catDao().getAll();
-                        if(cats.size() == 0) {
-
-                            Cat cat1 = new Cat("Cat one", DRAWABLE + "cat_one");
-                            Cat cat2 = new Cat("Cat two", DRAWABLE + "cat_two");
-                            Cat cat3 = new Cat("Cat three", DRAWABLE + "cat_three3");
-
-                            catDatabase.catDao().insert(cat1);
-                            catDatabase.catDao().insert(cat2);
-                            catDatabase.catDao().insert(cat3);
-                        }
-                    } finally {
-                        catDatabase.close();
-                    }
-                }
-            }).start();
-*/
         dbHelper = new DatabaseHelper(getApplicationContext());
         dbHelper.setUp();
 
         Button btn1 = (Button) findViewById(R.id.buttonQuiz);       //The first button is created´"Ta quiz"
-        btn1.setOnClickListener(new View.OnClickListener() {       //The first buttons action is created
+        btn1.setOnClickListener(new View.OnClickListener() {        //The first buttons action is created
             public void onClick(View v) {
                 //this should send the user to the quiz-activity
                 Intent intent = new Intent(MainActivity.this, Quiz.class);
@@ -70,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Button btn2 = (Button) findViewById(R.id.buttonBilde);       //The second button is created "Legg til bilde"
-        btn2.setOnClickListener(new View.OnClickListener() {    //The second button´s action is created
+        Button btn2 = (Button) findViewById(R.id.buttonAddImageMain);       //The second button is created "Add Image"
+        btn2.setOnClickListener(new View.OnClickListener() {         //The second button´s action is created
             public void onClick(View v) {
                 // this should make it possible for user to add images from the phone´s library
                 Intent intent = new Intent(MainActivity.this, Add.class);
@@ -79,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn3 = (Button) findViewById(R.id.buttonDatabase);       //The third button is created "Legg til bilde"
-        btn3.setOnClickListener(new View.OnClickListener() {    //The third button´s action is created
+        Button btn3 = (Button) findViewById(R.id.buttonDatabase);       //The third button is created "Database"
+        btn3.setOnClickListener(new View.OnClickListener() {            //The third button´s action is created
             public void onClick(View v) {
                 // this should make it possible for user to look at all the images added to the quiz
                 Intent intent = new Intent(MainActivity.this, Database.class);
