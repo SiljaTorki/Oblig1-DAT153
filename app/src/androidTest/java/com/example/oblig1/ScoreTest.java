47e29649087cfafc657285ea2b73ee91b5c37951
h576347 <42640829+h576347@users.noreply.github.com>
2021-02-18 10:39:21 +0100

    Endret til engelske navn og lagt inn grundige kommentarer

--- app/src/androidTest/java/com/example/oblig1/ScoreTest.java
+++ app/src/androidTest/java/com/example/oblig1/ScoreTest.java
@@ -1,10 +1,8 @@
 package com.example.oblig1;
 
-import androidx.test.espresso.ViewAssertion;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,54 +13,51 @@ import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withSubstring;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class ScoreTest {
 
+    /*
+    * @Rule is used to determine which class is tested
+     */
     @Rule
     public ActivityScenarioRule<Quiz> activityRule =
             new ActivityScenarioRule<>(Quiz.class);
 
+
+    /*
+    * A test to check that the score in Quiz.java is updated
+    * when guessing the correct cat-name of the cat-image in the quiz
+    *
+    * First getCatName() from Quiz.java file is called
+    * Then checking the score is 0 before guessing
+    * After clicking "check answer" button, the score should be updated to 1
+     */
     @Test
     public void scoreIsCorrect() {
 
-            //Providing an answer for a random image in Quiz.class
-            //onView(withId(R.id.editText1))
-                   // .perform(typeText("Cat one"), closeSoftKeyboard());
-
-            //Clicks the button i order to check whether the answer is correct or not
-            //onView(withId(R.id.buttonSvar))
-                   // .perform(click());
-
-            /*
-            * If it is correct guessed, then the score should update to 1
-            * otherwise it should not be updated
-            * only the first image is tested
-             */
-            /*if(onView(withId(R.id.imageView)).equals("android.resource://com.example.oblig1/drawable/cat_one")){
-                assertTrue(onView(withId(R.id.quizScore)).equals("Your score: 1"));
-            }else{
-                assertFalse(onView(withId(R.id.quizScore)).equals("Your score: 1"));
-            }*/
-
         String rightName = Quiz.getCatName();
         onView(withId(R.id.quizScore)).check(matches(withText("Your score: " + 0 )));
-        onView(withId(R.id.editText1)).perform(typeText(rightName), closeSoftKeyboard());
-        onView(withId(R.id.buttonSvar)).perform(click());
+        onView(withId(R.id.editTextGuess)).perform(typeText(rightName), closeSoftKeyboard());
+        onView(withId(R.id.buttonCheckAnswer)).perform(click());
         onView(withId(R.id.quizScore)).check(matches(withText("Your score: " + 1 )));
 
     }
 
+    /*
+     * A test to check that the score in Quiz.java is NOT updated
+     * when providing a wrong guess for the cat-name of the cat-image in the quiz
+     *
+     * First getCatName() from Quiz.java file is called
+     * Then checking the score is 0 before guessing
+     * After clicking "check answer" button, the score should stay at 0
+     */
     @Test
     public void scoreIsWrong() {
-        String rightName = Quiz.getCatName();
         onView(withId(R.id.quizScore)).check(matches(withText("Your score: " + 0 )));
-        onView(withId(R.id.editText1)).perform(typeText("Hei"), closeSoftKeyboard());
-        onView(withId(R.id.buttonSvar)).perform(click());
+        onView(withId(R.id.editTextGuess)).perform(typeText("Hei"), closeSoftKeyboard());
+        onView(withId(R.id.buttonCheckAnswer)).perform(click());
         onView(withId(R.id.quizScore)).check(matches(withText("Your score: " + 0 )));
     }
 
