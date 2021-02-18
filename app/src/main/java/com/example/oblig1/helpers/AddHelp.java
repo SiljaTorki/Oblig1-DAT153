81b5bbf912f7b7dff6f9a247fe342c5f38b2fcb6
h576347 <42640829+h576347@users.noreply.github.com>
2021-02-18 10:46:29 +0100

    Lagt inn en hjelpemetode for å sjekke at både navn og bilde er med--> er ikke idiotsikker

--- app/src/main/java/com/example/oblig1/helpers/AddHelp.java
+++ app/src/main/java/com/example/oblig1/helpers/AddHelp.java
@@ -1,20 +1,26 @@
 package com.example.oblig1.helpers;
 
 import android.graphics.Bitmap;
-
-//import com.example.oblig1.domain.CatList;
+import android.net.Uri;
 
 public class AddHelp {
+    private boolean addingOK = false;
 
-    public String responseUser(String name, Bitmap image){
+    public String responseUser(String name, String image){
        String response;
        //Method for adding the new cat-image
-        if(!name.equals("") && image != null) {
+        if(!name.trim().equals("") && !image.trim().equals("")) {
+            System.out.println("ADDED");
             response = "Image is added!";                //Toast-text if image is added
+            addingOK = true;
         }else{
             response = "Please add text and/or image";   //Toast-text if name or image is missing
         }
         return response;
+    }
+
+    public boolean readyForAdding(){
+        return addingOK;
     }
 
 }
