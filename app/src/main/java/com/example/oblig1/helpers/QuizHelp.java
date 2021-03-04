8a4c22bce8b0a172236827842b46263d3e120652
h576347 <42640829+h576347@users.noreply.github.com>
2021-03-04 13:45:48 +0100

    Fjernet en ubrukt metode for å lage en random liste av katter.

--- app/src/main/java/com/example/oblig1/helpers/QuizHelp.java
+++ app/src/main/java/com/example/oblig1/helpers/QuizHelp.java
@@ -1,25 +1,13 @@
 package com.example.oblig1.helpers;
 
-import com.example.oblig1.domain.Cat;
-import com.example.oblig1.sqlDAOs.CatDao;
-//import com.example.oblig1.domain.CatList;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
 * This class is helping QuizActivity.java
 *  - checking that the user´s guess
-*  - creates a random list of cats
-*  - return the amount of correct answers
+*  - return the number of correct answers
  */
 public class QuizHelp {
 
-    private List<Cat> original;
-    private List<Cat> cats;
     private int correct = 0;
-
-    private CatDao catDao;
 
     /*
     * Returns a response/answer to user´s guess, used in a toast
@@ -36,27 +24,7 @@ public class QuizHelp {
         return response;
 
     }
-    /*
-    * Creates a random list of cats
-    * method is called in btnNext onClickListener
-    */
-    public List<Cat> randomList(List<Cat> original) {
-        // Get the full list from data structure
-        //original = CatList.getCatList();
-        int tall;
 
-        cats = new ArrayList<Cat>(original.size());
-        //Loop that generates a random list
-        for(int j = 0; j < original.size(); j++) {
-            tall = (int) (Math.random()*original.size());
-            if(!cats.contains(original.get(tall)))           //Check if it already exist
-                cats.add(j, original.get(tall));             //AddActivity to the list
-            else
-                j--;      //To secure that all cats are transferred to the new list
-        }
-        return cats;
-
-    }
     /*
     * To get the number of correct answers
     * Method is called in btnCheckAnswer
